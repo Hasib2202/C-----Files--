93dd567a8e44d0a8991f5c9b3b225349555d7086
MD MOSTOFA HASIB <90277109+Hasib2202@users.noreply.github.com>
2024-08-29 22:30:13 +0600

    C++

---
+++ 4. Formatting_Output/4. Formatting_Output/rectangleArea.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float width, height, area;
+
+    cout<<"Enter width:";
+    cin>>width;
+
+
+    cout<<"Enter height:";
+    cin>>height;
+
+    area=width*height;
+
+    cout<<"area of rectangle is:"<<area;
+
+    return 0;
+
+
+}
+
